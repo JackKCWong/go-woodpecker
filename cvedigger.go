e07d30e57036c8c59a4325f4c6d6cce684d69bd5
JackKCWong <jiajue.huang@live.com>
2022-01-30 09:51:34 +0800

    skip multi modules projects for now

--- cvedigger.go
+++ cvedigger.go
@@ -7,30 +7,24 @@ type DigOpts struct {
 }
 
 func (w Woodpecker) Dig(args []string, opts DigOpts) error {
+	if multiModules, err := w.DepMgr.IsMultiModules(); err != nil {
+		return fmt.Errorf("failed to check if multi-modules: %w", err)
+	} else if multiModules {
+		return fmt.Errorf("dig does not work on the parent project. Please run it on the child project")
+	}
+
 	depTree, err := w.DepMgr.DependencyTree()
 	if err != nil {
 		return err
 	}
 
-	subtree, target, found := depTree.CriticalOrHigh()
+	subtree, cve, found := depTree.CriticalOrHigh()
 	if !found {
 		fmt.Println("Congratulations! Your project has no Critical or High CVE.")
 		return nil
 	}
 
-	fmt.Printf("%s found in %+v\n", target.ID, subtree.Root())
+	fmt.Printf("%s found in %+v\n", cve.ID, subtree.Root())
 
-	if subtree.Root().Depth == 0 {
-		subw := w
-		dm, err := subw.DepMgr.SubModule(subtree.Root().ID)
-		if err != nil {
-			return fmt.Errorf("failed to get sub-module %s: %s", subtree.Root().ID, err)
-		}
-
-		subw.DepMgr = dm
-
-		return subw.Kill([]string{target.ID}, opts.KillOpts)
-	} else {
-		return w.Kill([]string{target.ID}, opts.KillOpts)
-	}
+	return w.Kill([]string{cve.ID}, opts.KillOpts)
 }
